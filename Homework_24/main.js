232bdc34faa9186aaa67ad7da786eeb3309cdf25
Ihor Perzhun <iperzhun@gmail.com>
2023-03-30 19:48:25 +0400

    Homework_24

---
+++ Homework_24/main.js
@@ -0,0 +1,34 @@
+const name = prompt('Name');
+alert('Hello  ' + name);
+
+
+const birthyear = prompt('Enter year of birth');
+const curentyear = ('2023');
+alert(curentyear - birthyear);
+
+const side = prompt('Enter the lenght of side of the square');
+const quantity = ('4');
+alert(side * quantity);
+
+const radius = prompt('Enter radius of circle');
+const pi = ('3.14');
+alert(2 * pi * radius);
+
+const distance = prompt('Enter the distance in km');
+const time = prompt('Enter estimated time in hours');
+alert(distance / time);
+
+const dollar_amount = prompt('Enter dollar amount');
+const exchange_rate = (0.92);
+alert(dollar_amount * exchange_rate);
+
+
+
+
+
+
+
+
+
+
+

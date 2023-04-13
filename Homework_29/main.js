cbc581a165c8d9f3594bf1675cac5556236c31ad
Ihor Perzhun <iperzhun@gmail.com>
2023-04-13 19:49:51 +0400

    Yjmework_29

--- Homework_29/main.js
+++ Homework_29/main.js
@@ -9,37 +9,39 @@
 
 
 const shoppingList = [
-  {name: 'Bread', quantity: 3, costOfOne: 45, summ: 135, purchaseStatus: true},
-  {name: 'Water', quantity: 2, costOfOne: 28, summ: 56, purchaseStatus: false},
-  {name: 'Meat', quantity: 3, costOfOne: 320, summ: 960, purchaseStatus: true},
-  {name: 'Tomato', quantity: 3, costOfOne: 70, summ: 210, purchaseStatus: false},
-  {name: 'Banana', quantity: 4, costOfOne: 63, summ: 252, purchaseStatus: true},
-  {name: 'Potato', quantity: 8, costOfOne: 30, summ: 240, purchaseStatus: false}
+  {name: 'Bread', quantity: 3, price: 45, amount: 135, bought: true},
+  {name: 'Water', quantity: 2, price: 28, amount: 56, bought: false},
+  {name: 'Meat', quantity: 3, price: 320, amount: 960, bought: true},
+  {name: 'Tomato', quantity: 3, price: 70, amount: 210, bought: false},
+  {name: 'Banana', quantity: 4, price: 63, amount: 252, bought: true},
+  {name: 'Potato', quantity: 8, price: 30, amount: 240, bought: false}
 ];
 
 
 function displayList(list) {
   console.log("Shopping List:");
-  // sort the list by whether it's bought or not
-  list.sort((a, b) => (a.purchaseStatus === b.purchaseStatust ? 0 : a.purchaseStatus ? 1 : -1));
+  
+  list.sort((a, b) => (a.bought === b.bought ? 0 : a.bought ? 1 : -1));
   list.forEach((item) => {
-    const status = item.purchaseStatus ? " (Bought)" : "";
-    console.log(`- ${item.name} x${item.quantity}${status} - $${item.amount.toFixed(2)}`);
+    const status = item.bought ? " (Bought)" : "";
+    console.log(`  ${item.name} *${item.quantity}${status} - ${item.amount} UAH`);
   });
 }
 
-// Function to mark a product as bought
+
+
 function markAsBought(list, name) {
   const item = list.find((item) => item.name === name);
   if (item) {
-    item.purchaseStatus = true;
+    item.bought = true;
     console.log(`Marked ${name} as bought.`);
   } else {
     console.log(`Error: ${name} not found in the shopping list.`);
   }
 }
 
-// Example usage
-displayList(shoppingList); // display the list before making any changes
-markAsBought(shoppingList, "Milk"); // mark "Milk" as bought
-displayList(shoppingList); // display the updated list
+
+
+displayList(shoppingList); 
+markAsBought(shoppingList, "Bread"); 
+markAsBought(shoppingList, "Vodka");

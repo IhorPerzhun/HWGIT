4e1a5d440328e04e0967ca3e03bd43c339ecc8e5
Ihor Perzhun <iperzhun@gmail.com>
2023-04-26 17:42:24 +0400

    Homework_31

--- Homework_31/main.js
+++ Homework_31/main.js
@@ -1,26 +1,30 @@
-const showText = document.getElementById("showText");
-document.addEventListener("keydown", function(e) {
+const API = "http://www.omdbapi.com/?i=tt3896198&apikey=33a1b2"
 
-  if (e.ctrlKey && e.code === "KeyE") {
-    e.preventDefault();
 
-    const textArea = document.createElement("textarea");
-    textArea.value = showText.innerText;
+const apiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=33a1b2";
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
 
-    showText.replaceWith(textArea);
+// Get form elements
+const searchForm = document.getElementById('search-form');
+const movieNameInput = document.getElementById('searchInput');
+const movieTypeInput = document.getElementById('typeSelect');
 
-    document.addEventListener("keydown", function(e) {
-      if (e.ctrlKey && e.code === "KeyS") {
-        e.preventDefault();
-
-        const newText = document.createElement("div");
-        newText.style.color = "green";
-        newText.innerText = textArea.value;
-        
-        textArea.replaceWith(newText);
-
-        document.removeEventListener("keydown");
-      }
+// Add event listener to form submit button
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  // Get search criteria
+  const movieName = movieNameInput.value;
+  const movieType = movieTypeInput.value;
+  
+  // Send AJAX request
+  const url = `${apiUrl}&s=${movieName}&type=${movieType}`;
+  fetch('${apiKey}')
+    .then(response => response.json())
+    .then(data => {
+      // Handle response data
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-  }
 });

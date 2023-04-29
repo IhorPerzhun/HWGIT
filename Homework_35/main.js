ac960323041241134d5706fc5e5b407141f29b5e
Ihor Perzhun <iperzhun@gmail.com>
2023-04-30 01:42:04 +0400

    scroll

---
+++ Homework_35/main.js
@@ -0,0 +1,46 @@
+'use strict'
+const link = document.querySelectorAll('.nav__link')
+const contact = document.querySelector('.contactBox')
+const section = document.querySelectorAll('section')
+const arrow = document.querySelector('.scroll')
+
+link[1].addEventListener('click', function(e){
+    console.log('click');
+    
+    
+    const rect = section[0].getBoundingClientRect();
+    window.scrollTo({
+        top: rect.top
+    })
+});
+
+link[2].addEventListener('click', function(e){
+    console.log('click');
+    
+    
+    const rect = section[1].getBoundingClientRect();
+    window.scrollTo({
+        top: rect.top
+    })
+});
+
+
+link[3].addEventListener('click', function(e){
+    console.log('click');
+    
+    
+    const rect = contact.getBoundingClientRect();
+    window.scrollTo({
+        top: rect.top
+    })
+});
+
+arrow.addEventListener('click', function(e){
+    console.log('click');
+    
+    
+    const rect = section[0].getBoundingClientRect();
+    window.scrollTo({
+        top: rect.top
+    })
+});
